50c735e975a2ae93bbbd9ca624be592d4d8f2ac2
lee.eunji <fdmswl@gmail.com>
2025-07-11 19:20:51 +0900

    chore: 상수 적용

--- src/shared/ui/progressbar/progressBarVariants.ts
+++ src/shared/ui/progressbar/progressBarVariants.ts
@@ -1,11 +1,12 @@
+import { PROGRESS_STATE } from '@/shared/types/progressStatus';
 import { cva } from 'class-variance-authority';
 
 export const progressFill = cva('h-full transition-all duration-500 rounded-md', {
   variants: {
     state: {
-      open: 'bg-orange-600',
-      confirmed: 'bg-primary', // orange-500
-      full: 'bg-orange-400',
+      [PROGRESS_STATE.OPEN]: 'bg-orange-600',
+      [PROGRESS_STATE.CONFIRMED]: 'bg-primary', // orange-500
+      [PROGRESS_STATE.FULL]: 'bg-orange-400',
     },
   },
 });
