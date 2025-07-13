d4eca25a544b6748bf2e3a2e77d39aa1afeba5bc
LEE MYEONGUK <muk525@icloud.com>
2025-07-13 17:27:11 +0900

    fix: 필터, 정렬 컴포넌트의 드롭다운 z-index를 디자인 토큰으로 변경

--- src/shared/ui/sort/Sort.tsx
+++ src/shared/ui/sort/Sort.tsx
@@ -125,7 +125,7 @@ export const Sort = ({
         </span>
       </button>
       {open && (
-        <div className="absolute right-0 z-10 mt-2 min-w-32 rounded-lg border border-gray-200 bg-white">
+        <div className="absolute right-0 z-[var(--z-dropdown)] mt-2 min-w-32 rounded-lg border border-gray-200 bg-white">
           {options.map((option, idx) => (
             <button
               key={option.value}
