9ac10a5b074c5005952196c25f53217d5535bc2f
LEE MYEONGUK <muk525@icloud.com>
2025-07-17 13:14:24 +0900

    feat: 찜한 페이지 클라이언트 컴포넌트 변경

--- src/app/[locale]/favorites/page.tsx
+++ src/app/[locale]/favorites/page.tsx
@@ -1,13 +1,9 @@
-import { getTranslations } from 'next-intl/server';
-import { Locale } from '@/i18n';
+'use client';
 
-interface FavoritesPageProps {
-  params: Promise<{ locale: Locale }>;
-}
+import { useTranslations } from 'next-intl';
 
-export default async function FavoritesPage({ params }: FavoritesPageProps) {
-  const { locale } = await params;
-  const t = await getTranslations({ locale, namespace: 'pages.favorites' });
+export default function FavoritesPage() {
+  const t = useTranslations('pages.favorites');
 
   return (
     <div className="flex flex-1 items-center justify-center">
