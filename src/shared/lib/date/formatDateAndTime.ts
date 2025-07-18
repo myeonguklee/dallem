6c5d54861b645aae6462d6fcdc30f3bdc529b724
JunWoo Park <97427744+always97@users.noreply.github.com>
2025-07-18 16:42:00 +0900

    feat: datetime을 연월일로 출력 양식 추가

--- src/shared/lib/date/formatDateAndTime.ts
+++ src/shared/lib/date/formatDateAndTime.ts
@@ -2,13 +2,18 @@ export function formatDateAndTime(isoString: string) {
   const date = new Date(isoString);
   date.setHours(date.getHours() + 9); // KST 보정
 
+  const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
   const hours = date.getHours();
   const minutes = date.getMinutes();
 
+  // 2025-01-01T00:00:00Z -> 2025-01-01
+  const formattedYearMonthDay = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
+  // 2025-01-01T00:00:00Z -> 1월 1일
   const formattedDate = `${month}월 ${day}일`;
+  // 2025-01-01T07:09:00Z -> 07:09
   const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
 
-  return { formattedDate, formattedTime };
+  return { formattedYearMonthDay, formattedDate, formattedTime };
 }
