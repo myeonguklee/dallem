62fffaaab3ce2ad119b1e7c49251d7f0ac970743
LEE MYEONGUK <muk525@icloud.com>
2025-07-21 10:25:45 +0900

    feat: 모임 찾기 페이지 소개 섹션

--- src/app/[locale]/gathering/page.tsx
+++ src/app/[locale]/gathering/page.tsx
@@ -1,5 +1,7 @@
 import { getTranslations } from 'next-intl/server';
 import { Locale } from '@/i18n';
+import { Button } from '@/shared/ui/button';
+import { DoubleHeartIcon } from '@/shared/ui/icon';
 
 interface GatheringPageProps {
   params: Promise<{ locale: Locale }>;
@@ -10,7 +12,19 @@ export default async function GatheringPage({ params }: GatheringPageProps) {
   const t = await getTranslations({ locale, namespace: 'pages.gatherings' });
 
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex flex-1 flex-col gap-4">
+      <div className="mt-10 flex h-20 w-full items-center gap-4">
+        <DoubleHeartIcon />
+        <div className="flex h-full flex-col justify-evenly">
+          <p className="text-sm font-medium">함께 할 사람이 없나요?</p>
+          <p className="text-2xl font-semibold">지금 모임에 참여해보세요</p>
+        </div>
+      </div>
+      <div className="flex justify-end">
+        <Button className="">모임 만들기</Button>
+      </div>
+
+      <div></div>
       <div className="text-2xl font-bold">{t('title')}</div>
     </div>
   );
