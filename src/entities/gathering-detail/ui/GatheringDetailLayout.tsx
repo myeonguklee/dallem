965241f87d01534f0dacf43a797f23814fa4bd2b
JunWoo Park <97427744+always97@users.noreply.github.com>
2025-07-22 18:52:07 +0900

    feat: 상세페이지 mock데이터 삭제 및 실제 api 연동

--- src/entities/gathering-detail/ui/GatheringDetailLayout.tsx
+++ src/entities/gathering-detail/ui/GatheringDetailLayout.tsx
@@ -1,101 +1,46 @@
 'use client';
 
 import Image from 'next/image';
+import { useGetGatheringDetail } from '@/entities/gathering-detail/api/queries';
+import { GatheringDeadlineTag } from '@/entities/gathering/ui';
+import { useGetParticipants } from '@/entities/participant/api/queries';
 import { formatDateAndTime } from '@/shared/lib/date';
-import { Tag } from '@/shared/ui/tag';
 import { BottomFloatingBar, GatheringRole } from '@/widgets/BottomFloatingBar';
 import { ContainerInformation } from '@/widgets/ContainerInformation';
-import capProfileImage from '@/widgets/ContainerInformation/cap-profile.jpg';
 import { ReviewList } from './ReviewList';
 
-export const GatheringDetailLayout = ({ id }: { id: string }) => {
-  // const { data: gathering, isLoading, error } = useGetGatheringDetail(Number(id));
-  // if (isLoading) return <p>로딩 중...</p>;
-  // if (error) return <p>에러 발생</p>;
-  // if (!gathering) return null;
-  // const { data: participantsData } = useGetParticipants(Number(id));
+export const GatheringDetailLayout = ({ id }: { id: number }) => {
+  const { data: gathering } = useGetGatheringDetail(id);
 
-  const gathering = {
-    teamId: '1',
-    id: 1,
-    type: '친목',
-    name: '달램핏 오피스 스트레칭',
-    dateTime: '2025-07-17T19:30:00Z',
-    registrationEnd: '2025-07-19T17:00:00Z',
-    location: '을지로 3가',
-    participantCount: 4,
-    capacity: 10,
-    image: '/gathering-banner.jpg',
-    createdBy: 123,
-    canceledAt: null,
-  };
-  const participantsData = [
-    {
-      teamId: 1,
-      userId: 1,
-      gatheringId: 100,
-      joinedAt: '2025-07-21T10:00:00Z',
-      User: {
-        id: 1,
-        email: 'alice@example.com',
-        name: 'Alice',
-        companyName: 'OpenAI',
-        image: '/cat-profile.jpg',
-      },
-    },
-    {
-      teamId: 1,
-      userId: 2,
-      gatheringId: 100,
-      joinedAt: '2025-07-21T10:01:00Z',
-      User: {
-        id: 2,
-        email: 'bob@example.com',
-        name: 'Bob',
-        companyName: 'Google',
-        image: '/cat-profile.jpg',
-      },
-    },
-    {
-      teamId: 1,
-      userId: 3,
-      gatheringId: 100,
-      joinedAt: '2025-07-21T10:02:00Z',
-      User: {
-        id: 3,
-        email: 'carol@example.com',
-        name: 'Carol',
-        companyName: 'Meta',
-        image: '/cat-profile.jpg',
-      },
-    },
-  ];
-  const participants = participantsData.map(({ userId, User }) => ({
+  const { data: participantsData } = useGetParticipants(id);
+
+  const participants = participantsData!.map(({ userId, User }) => ({
     id: String(userId),
-    image: User.image,
+    image:
+      User.image ||
+      'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/together-dallaem/1728361169610_19610008.JPG',
   }));
 
   const { formattedDate, formattedTime } = formatDateAndTime(gathering.dateTime);
-  console.log('Gathering ID:', id);
+
   return (
-    <main className="tablet:pb-[84px] flex flex-col items-center px-4 py-8 pb-0">
+    <main className="tablet:mb-0 mb-[100px] flex flex-col items-center px-4 py-8">
       <section className="mb-8 flex w-full max-w-[996px] flex-col gap-4 md:flex-row">
         {/* Gathering Banner */}
         <div className="relative h-[270px] w-[486px] overflow-hidden rounded-xl">
           <Image
-            src={capProfileImage}
+            src={gathering.image}
             alt={'alt'}
             fill
             className="object-cover"
             sizes="(max-width: 768px) 100vw, 486px"
           />
-          <div className="absolute top-2 right-2 z-10">
-            <Tag tagColor={'primary'}>오늘 {'21'}시 마감</Tag>
-          </div>
+          {/* Tag */}
+          <GatheringDeadlineTag registrationEnd={new Date(gathering.registrationEnd)} />
         </div>
         {/* ContainerInformation 컴포넌트 */}
         <ContainerInformation
-          title={gathering.name}
+          title={gathering.name || '제목 없음'}
           location={gathering.location}
           date={formattedDate}
           time={formattedTime}
@@ -107,7 +52,7 @@ export const GatheringDetailLayout = ({ id }: { id: string }) => {
       </section>
       <section className="h-[687px] w-full max-w-[996px] border-t-2 border-gray-300 bg-gray-50 p-4">
         <h2 className="mb-4 text-xl font-semibold">생생 후기</h2>
-        <ReviewList />
+        <ReviewList id={id} />
       </section>
       {/* 하단 플로팅 바 */}
       <BottomFloatingBar
--- src/entities/gathering-detail/ui/ReviewList.tsx
+++ src/entities/gathering-detail/ui/ReviewList.tsx
@@ -1,42 +1,42 @@
+import { useState } from 'react';
+import { useGatheringReviewList } from '@/entities/review/hooks/useGatheringReviewList';
 import { ReviewCard } from '@/entities/review/ui/ReviewCard';
+import { Pagination } from '@/shared/ui/pagination';
 
-export const ReviewList = () => {
-  // 여기에 실제 리뷰 데이터를 가져오는 로직이 들어갈 예정
-  // 예시로 더미 데이터
-  // const reviews = useReviews();
-  const reviews = [
-    {
-      score: 5,
-      comment: '정말 유익한 모임이었어요!',
-      dateTime: '2025-05-29T12:00:00Z',
-      userName: '사용자1',
-      gatheringName: '코딩 스터디',
-      location: '서울',
-    },
-    {
-      score: 4,
-      comment: '다음에도 꼭 참여하고 싶어요!',
-      dateTime: '2025-05-29T12:00:00Z',
-      userName: '사용자2',
-      gatheringName: '디자인 워크숍',
-      location: '부산',
-    },
-  ];
+export const ReviewList = ({ id }: { id: number }) => {
+  // 페이지당 리뷰 개수 설정
+  // 상수 코드 추후 분리 ?
+  const PAGE_SIZE = 4;
+
+  const [page, setPage] = useState(1);
+  const offset = (page - 1) * PAGE_SIZE;
+
+  const { data } = useGatheringReviewList(id, {
+    offset,
+    limit: PAGE_SIZE,
+    sortBy: 'createdAt',
+    sortOrder: 'desc',
+  });
 
   return (
-    <div className="flex flex-col gap-4">
-      {/* 여기에 ReviewCard 컴포넌트들을 맵핑해서 넣을 예정 */}
-      {reviews.map((review, index) => (
+    <>
+      {data?.data.map((review) => (
         <ReviewCard
-          key={index}
+          key={review.id}
           score={review.score}
           comment={review.comment}
-          dateTime={review.dateTime}
-          userName={review.userName}
-          gatheringName={review.gatheringName}
-          location={review.location}
+          dateTime={review.createdAt}
+          userName={review.User.name}
+          gatheringName={review.Gathering.name}
+          location={review.Gathering.location}
         />
       ))}
-    </div>
+
+      <Pagination
+        currentPage={page}
+        totalPages={data?.totalPages ?? 1}
+        onPageChange={setPage}
+      />
+    </>
   );
 };
