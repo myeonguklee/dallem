6da509982d994596c7637011a9e3f47f1c942508
lee.eunji <fdmswl@gmail.com>
2025-07-23 15:26:16 +0900

    chore: Input 변경사항 storybook 반영

--- src/shared/ui/input/Input.tsx
+++ src/shared/ui/input/Input.tsx
@@ -15,7 +15,7 @@ export const INPUT_VARIANT = {
   typing: 'border-2 border-orange-600 text-gray-800',
   done: 'border-2 border-gray-50 text-gray-800',
   error: 'border-2 border-red-600 text-gray-800',
-};
+} as const;
 
 export const inputVariants = cva(
   'flex w-full rounded-xl px-4 py-2.5 outline-none transition bg-gray-50 text-sm leading-5 tablet:text-base tablet:leading-6',
