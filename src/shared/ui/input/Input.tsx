c0e4bb21ffdd658287881b93823c03bada9904d1
lee.eunji <fdmswl@gmail.com>
2025-07-23 15:26:13 +0900

    fix(Input): 사이즈 고정 해제, 폰트 사이즈 조정
    mobile 에서 폰트사이즈 sm(14px) 태블릿 이상에서 base(16px) 적용

--- src/shared/ui/input/Input.tsx
+++ src/shared/ui/input/Input.tsx
@@ -8,36 +8,39 @@ export interface InputProps
   errorMessage?: string;
 }
 
-const inputVariants = cva('flex w-full rounded-xl px-4 py-2.5 outline-none transition bg-gray-50', {
-  variants: {
-    variant: {
-      default: 'text-gray-400',
-      toggle: 'border border-gray-50 text-gray-400', // dropdown 완성 후 추가 개발
-      hover: 'border-2 border-orange-300 text-gray-400',
-      typing: 'border-2 border-orange-600 text-gray-800',
-      done: 'border-2 border-gray-50 text-gray-800',
-      error: 'border-2 border-red-600 text-gray-400',
-      password_default: 'text-gray-400',
-      password_off: 'text-gray-800',
-      password_on: 'text-gray-800',
-      password_error: 'border-2 border-red-600 text-gray-800',
+const inputVariants = cva(
+  'flex w-full rounded-xl px-4 py-2.5 outline-none transition bg-gray-50 text-sm leading-5 tablet:text-base tablet:leading-6',
+  {
+    variants: {
+      variant: {
+        default: 'text-gray-400',
+        toggle: 'border border-gray-50 text-gray-400', // dropdown 완성 후 추가 개발
+        hover: 'border-2 border-orange-300 text-gray-400',
+        typing: 'border-2 border-orange-600 text-gray-800',
+        done: 'border-2 border-gray-50 text-gray-800',
+        error: 'border-2 border-red-600 text-gray-400',
+        password_default: 'text-gray-400',
+        password_off: 'text-gray-800',
+        password_on: 'text-gray-800',
+        password_error: 'border-2 border-red-600 text-gray-800',
+      },
+      inputSize: {
+        lg: 'text-base leading-6',
+        sm: 'text-sm leading-5',
+      },
     },
-    inputSize: {
-      lg: 'text-base leading-6',
-      sm: 'text-sm leading-5',
+    defaultVariants: {
+      variant: 'default',
+      inputSize: 'lg',
     },
   },
-  defaultVariants: {
-    variant: 'default',
-    inputSize: 'lg',
-  },
-});
+);
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ variant, inputSize, errorMessage, ...props }, ref) => {
     return (
       // figma에 따라 w-[460px]로 설정. 개발 진행에 따라 w-full로 변경할 수 있음.
-      <div className="w-[460px]">
+      <>
         <div className={`${inputVariants({ variant, inputSize })}`}>
           <input
             ref={ref}
@@ -57,7 +60,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
           )}
         </div>
         {errorMessage && <p className="pt-[8px] text-xs text-red-500">{errorMessage}</p>}
-      </div>
+      </>
     );
   },
 );
