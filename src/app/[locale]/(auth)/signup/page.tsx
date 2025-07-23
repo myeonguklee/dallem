3c4d23e889ee1a432a342c005a8c7c00ccabec1f
lee.eunji <fdmswl@gmail.com>
2025-07-23 15:26:14 +0900

    feat: signup page

--- src/app/[locale]/(auth)/signup/page.tsx
+++ src/app/[locale]/(auth)/signup/page.tsx
@@ -1,6 +1,4 @@
-'use client';
-
-import SignupForm from '@/features/signup/ui/SignupForm';
+import { SignupForm } from '@/features/signup/ui/SignupForm';
 
 export default function SignUpPage() {
   return <SignupForm />;
