59037c364582672dd0dfb7a3e963c4c0b50a42a3
lee.eunji <fdmswl@gmail.com>
2025-07-23 15:26:15 +0900

    chore: 주석 삭제

--- src/features/signup/utils/getSignupImageSize.ts
+++ src/features/signup/utils/getSignupImageSize.ts
@@ -1,9 +1,9 @@
 import { getBreakpointsPx } from '@/shared/lib/getBreakpointsPx';
 
 const IMAGE_SIZE_BY_BREAKPOINTS = {
-  mobile: 290, // 23.4375rem
-  tablet: 407, // 46.5rem
-  web: 588, // 75rem
+  mobile: 290,
+  tablet: 407,
+  web: 588,
 } as const;
 
 export const getSignupImageSize = (width: number): number => {
