785000ab64db6bbd205f74c31cc90bbcdba0cabd
JunWoo Park <97427744+always97@users.noreply.github.com>
2025-07-25 10:26:04 +0900

    fix: suspense 쿼리 훅 사용시 파라미터 유효성은 훅 호출하기 전에 검증하도록 수정

--- src/app/[locale]/gathering/[id]/page.tsx
+++ src/app/[locale]/gathering/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react';
 import { getTranslations } from 'next-intl/server';
+import { notFound } from 'next/navigation';
 import { GatheringDetailLayout } from '@/entities/gathering-detail/ui';
 import { Locale } from '@/i18n';
 
@@ -10,6 +11,11 @@ interface GatheringDetailPageProps {
 export default async function GatheringDetailPage({ params }: GatheringDetailPageProps) {
   const { id, locale } = await params;
   const t = await getTranslations({ locale, namespace: 'pages.gathering.detail' });
+
+  const numericId = Number(id);
+  if (isNaN(numericId)) {
+    notFound();
+  }
   console.log('GatheringDetailPage', { id, locale, t });
 
   return (
@@ -17,7 +23,7 @@ export default async function GatheringDetailPage({ params }: GatheringDetailPag
     // <ErrorBoundary fallback={<p>데이터를 불러오는 중 에러가 발생했습니다.</p>}>
     // </ErrorBoundary>
     <Suspense fallback={<p>모임 정보를 불러오는 중...</p>}>
-      <GatheringDetailLayout id={Number(id)} />
+      <GatheringDetailLayout id={numericId} />
     </Suspense>
   );
 }
