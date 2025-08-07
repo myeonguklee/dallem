a368224ca86bd238e4ef73936d69ec878e0d541a
JunWoo Park <97427744+always97@users.noreply.github.com>
2025-08-07 11:47:36 +0900

    feat: 스켈레톤 UI 테스트 코드 작성

--- src/shared/ui/skeleton/Skeleton.tsx
+++ src/shared/ui/skeleton/Skeleton.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/shared/lib';
 export const Skeleton = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => {
   return (
     <div
-      className={cn('animate-pulse rounded-md bg-gray-200 dark:bg-gray-700', className)}
+      className={cn('animate-pulse rounded-md bg-gray-200 dark:bg-gray-400', className)}
       {...props}
     />
   );
