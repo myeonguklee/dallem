bbb36abd352c14822cc495f62a1d7433c9fb25bb
JunWoo Park <97427744+always97@users.noreply.github.com>
2025-08-11 15:46:03 +0900

    feat: favorite 쿼리키가 별도로 존재하여 공통 쿼리키로 통합

--- src/entities/favorites/api/queries.ts
+++ src/entities/favorites/api/queries.ts
@@ -1,6 +1,6 @@
+import { QUERY_KEYS } from '@/shared/api';
 import { keepPreviousData, useInfiniteQuery } from '@tanstack/react-query';
 import { fetchFavoritesData } from '../lib/favorite';
-import { FAVORITES_QUERY_KEY } from './queryKeys';
 
 interface UseGetFavoritesGatheringProps {
   type: string;
@@ -14,7 +14,7 @@ export const useGetFavoritesGathering = ({
   enabled,
 }: UseGetFavoritesGatheringProps) => {
   return useInfiniteQuery({
-    queryKey: FAVORITES_QUERY_KEY.favorites.list({ type, favoriteIds }),
+    queryKey: QUERY_KEYS.favorites.list({ type, favoriteIds }),
     queryFn: ({ pageParam }) => {
       return fetchFavoritesData({ ids: favoriteIds, type, pageParam });
     },
