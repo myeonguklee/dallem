3bfc4d86f93981d6f8d8be32b7940eb3b47835fc
lee.eunji <fdmswl@gmail.com>
2025-08-11 16:54:19 +0900

    chore: label 아이디 -> 이메일

--- src/features/signin/model/signFields.ts
+++ src/features/signin/model/signFields.ts
@@ -2,11 +2,11 @@ import { AuthFormField } from '@/widgets/AuthForm/model/types';
 import { SigninFormData } from './type';
 
 export const signFields = [
-  { name: 'email', label: 'pages.signup.email', placeholder: 'pages.signup.emailPlaceholder' },
+  { name: 'email', label: 'pages.signin.email', placeholder: 'pages.signin.emailPlaceholder' },
   {
     name: 'password',
-    label: 'pages.signup.password',
-    placeholder: 'pages.signup.passwordPlaceholder',
+    label: 'pages.signin.password',
+    placeholder: 'pages.signin.passwordPlaceholder',
     type: 'password',
   },
 ] as const satisfies AuthFormField<SigninFormData>[];
